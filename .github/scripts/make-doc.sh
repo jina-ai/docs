9d6fc27d7ef2c3dd73ddf3dc267b8074d2385480
bwanglzu <bo.wang@jina.ai>
2021-01-29 18:37:18 +0100

    docs: install jina hub in make doc

--- .github/scripts/make-doc.sh
+++ .github/scripts/make-doc.sh
@@ -33,6 +33,7 @@ wget https://raw.githubusercontent.com/jina-ai/jina/master/RELEASE.md -P $(pwd)/
 wget https://raw.githubusercontent.com/jina-ai/jina/master/CHANGELOG.md -P $(pwd)/chapters/
 cd -
 # create markdown for List [X] drivers in Jina & List [X] executors in Jina to chapters/
+pip install "jina[hub]"
 cd ${DOC_DIR} && jina check --summary-driver chapters/all_driver.md && cat chapters/all_driver.md && cd -
 cd ${DOC_DIR} && jina check --summary-exec chapters/all_exec.md && cat chapters/all_exec.md && cd -
 cd ${DOC_DIR} && make html && cd -
